ad548be7e85025fb4ee6555d6420f637a496addb
Souradip <dgsouro77@gmail.com>
2024-06-22 23:56:39 +0530

    Updated the ui

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,7 +1,7 @@
 import Footer from '@/components/Footer';
 import LandingNavigation from '@/components/LandingNavigation';
 import { Button } from 'react-bootstrap';
-import SideImg from '../assets/feedback1.png';
+import SideImg from '../assets/feedback 3.jpg';
 import { Link } from 'react-router-dom';
 
 const LandingPage = () => {
@@ -12,11 +12,11 @@ const LandingPage = () => {
       <div className=" max-w-2xl mx-auto">
         <div className="p-4 ml-10 mt-20">
           <h1>
-            There is no failure.{' '}
-            <span className="text-violet-300">Only feedback.</span>
+            Join us in shaping tomorrow.{' '}
+            <span className="text-blue-500">Grateful for your insights.</span>
           </h1>
-          <p>“Feedback is the breakfast of champions.”</p>
-        <span className='font-thin text-xs'>  – Ken Blanchard</span>
+          <p>The only source of knowledge is experience</p>
+        <span className='font-thin text-xs'>  – Albert Einstein</span>
         </div>
 
         <div className="space-x-4 ml-20 ">
